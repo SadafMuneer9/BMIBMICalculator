1ccc76eca576a359dbfb2c5739a2b7db4aa983bf
Shivam-MoEngage <shivam.agrawal@moengage.com>
2022-04-08 18:06:34 +0530

    updated code

--- app/src/main/java/android/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/android/example/bmicalculator/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +13,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var height: EditText
     private lateinit var weight: EditText
     private lateinit var name: EditText
+    private lateinit var heightText: TextView
     private lateinit var radioGroup: RadioGroup
     private lateinit var radioCm: RadioButton
     private lateinit var radioInch: RadioButton
+    private lateinit var radioMt: RadioButton
+    private var conversionVal = 2;   // 0 = CM , 1 = inch , 2= Mt
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,22 +31,84 @@ class MainActivity : AppCompatActivity() {
         height = findViewById(R.id.Height)
         weight = findViewById(R.id.Weight)
         name = findViewById(R.id.name)
-        radioGroup=findViewById(R.id.radioGroup)
-        radioCm=findViewById(R.id.radioCm)
-        radioInch=findViewById(R.id.radioInch)
+        radioGroup = findViewById(R.id.radioGroup)
+        radioCm = findViewById(R.id.radioCm)
+        radioInch = findViewById(R.id.radioInch)
+        radioMt = findViewById(R.id.radioMt)
+        heightText = findViewById(R.id.tvHeight);
+
+
+        var checkedId = R.id.radioMt
+
+        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
+
+                checkedId = radioGroup.checkedRadioButtonId
+                conversionVal = findConversionVal(checkedId)
+            }
+
+        })
+
+
+
+
         btnCalculate.setOnClickListener {
 
             val heightInFloat = height.text.toString().toFloat()
-            val calculateHeight = heightInFloat * heightInFloat //height square
+
+            val finalHeight = findConversionMultiplier(conversionVal , heightInFloat)
+
+
+            //formula in meters
+            val calculateHeight = finalHeight * finalHeight
 
             val weight: Float = weight.text.toString().toFloat()
 
             val BMI = weight / calculateHeight
 
+            val roundoff = (BMI * 100.0).roundToInt() / 100.0
+
             Toast.makeText(this, "Calculated BMI $BMI", Toast.LENGTH_SHORT).show()
 
-            moveToOutPutScreen(name.text.toString(), BMI)
+            moveToOutPutScreen(name.text.toString(), roundoff.toFloat())
         }
+    }
+
+    private fun findConversionVal(checkedId: Int): Int {
+
+        if (checkedId == R.id.radioCm){
+            heightText.text = getString(R.string.Height_cm);
+            return 0
+        }else if (checkedId == R.id.radioInch){
+            heightText.text = getString(R.string.Height_in);
+            return 1
+        }
+        heightText.text = getString(R.string.Height_mt);
+        return 2
+    }
+
+    private fun findConversionMultiplier(conversionVal : Int, actualHeight : Float)  : Float{
+
+        var absoluteHeight = actualHeight
+
+        if (conversionVal == 0){
+
+            //cm
+
+            absoluteHeight = actualHeight / 100;
+
+
+        }else if (conversionVal == 1){
+
+            //inch
+
+            absoluteHeight = (actualHeight / 39.37).toFloat()
+
+        }else{
+            absoluteHeight = actualHeight
+        }
+
+        return absoluteHeight;
     }
 
     private fun moveToOutPutScreen(name: String, bmi: Float) {
--- app/src/main/java/android/example/bmicalculator/OutputScreen.kt
+++ app/src/main/java/android/example/bmicalculator/OutputScreen.kt
@@ -3,8 +3,10 @@ package android.example.bmicalculator
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class OutputScreen : AppCompatActivity() {
 
@@ -12,6 +14,8 @@ class OutputScreen : AppCompatActivity() {
     private lateinit var bmi: TextView
     private lateinit var reCalculate: Button
     private lateinit var bmiStatus: String
+    private lateinit var bmiInfo: TextView
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +24,17 @@ class OutputScreen : AppCompatActivity() {
         name = findViewById(R.id.output_name)
         bmi = findViewById(R.id.tvResult)
         reCalculate = findViewById(R.id.reCalculate)
+        bmiInfo = findViewById(R.id.bmiInfo)
+        imageView = findViewById(R.id.bodyTypeImage)
+
+
         val intent = intent
 
         name.text = intent.getStringExtra("userName")
-        bmi.text = intent.getFloatExtra("userBMI", 0f).toString()
+        val Bmi = intent.getFloatExtra("userBMI", 0f)
+        bmi.text = Bmi.toString()
+
+        bmiInfo.text = bmiStatusValue(Bmi)
 
         reCalculate.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -32,12 +43,18 @@ class OutputScreen : AppCompatActivity() {
 
     }
     private fun bmiStatusValue(bmi: Float): String {
-        if (bmi < 18.5)
+        if (bmi < 18.5) {
+            imageView.setBackgroundResource(R.drawable.underweight)
             bmiStatus = "Underweight"
-        else if (bmi >= 18.5 && bmi < 25)
+        }
+        else if (bmi >= 18.5 && bmi < 25){
+            imageView.setBackgroundResource(R.drawable.normal)
             bmiStatus = "Normal"
-        else if (bmi >= 25 && bmi < 30)
+        }
+        else if (bmi >= 25 && bmi < 30) {
+            imageView.setBackgroundResource(R.drawable.normal)
             bmiStatus = "Overweight"
+        }
         else if (bmi > 30)
             bmiStatus = "Obese"
         return bmiStatus
